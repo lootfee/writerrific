38db0ef1b4a4158cab2d71c7f33863b849952143
lootfee <lutfiayven@gmail.com>
2019-12-12 02:14:06 +0400

    temp fix redirect issue

--- app/routes.py
+++ app/routes.py
@@ -272,12 +272,12 @@ def project_synopsis(id, title):
 @login_required
 def delete_project(id):
 	project = Project.query.filter_by(id=id).first()
-	if current_user.is_anonymous:
+	'''if current_user.is_anonymous:
 		return redirect(url_for('index'))
 	if current_user.is_authenticated:
 		if current_user.is_superuser() == False:
 			if project.author is not current_user:
-				return redirect(url_for('index'))
+				return redirect(url_for('index'))'''
 	db.session.delete(project)
 	for p in project.chapters:
 		session.delete(p)
@@ -287,11 +287,11 @@ def delete_project(id):
 @app.route('/delete_project/<id>', methods=['GET', 'POST'])
 @login_required
 def quarantine_project(id):
-	if current_user.is_anonymous:
+	'''if current_user.is_anonymous:
 		return redirect(url_for('index'))
 	if current_user.is_authenticated:
 		if current_user.is_superuser() == False:
-			return redirect(url_for('index'))
+			return redirect(url_for('index'))'''
 	project = Project.query.filter_by(id=id).first()
 	project.date_quarantined = datetime.utcnow()
 	db.session.commit()
@@ -300,11 +300,11 @@ def quarantine_project(id):
 @app.route('/unpublish_project/<id>', methods=['GET', 'POST'])
 @login_required
 def unpublish_project(id):
-	if current_user.is_anonymous:
+	'''if current_user.is_anonymous:
 		return redirect(url_for('index'))
 	if current_user.is_authenticated:
 		if current_user.is_superuser() == False:
-				return redirect(url_for('index'))
+				return redirect(url_for('index'))'''
 	project = Project.query.filter_by(id=id).first()
 	project.date_published = None
 	db.session.commit()
@@ -333,12 +333,12 @@ def downvote(id):
 def chapter(id):
 	chapter = Chapter.query.filter_by(id=id).first()
 	project = Project.query.filter_by(id=chapter.project_id).first()
-	if current_user.is_anonymous:
+	'''if current_user.is_anonymous:
 		return redirect(url_for('index'))
 	if current_user.is_authenticated:
 		if current_user.is_superuser() == False:
 			if project.author is not current_user:
-				return redirect(url_for('index'))
+				return redirect(url_for('index'))'''
 	form = EditChapterForm()
 	if form.validate_on_submit():
 		chapter.chapter_no = form.edit_chapter_number.data
@@ -353,12 +353,12 @@ def chapter(id):
 def delete_chapter(id):
 	chapter = Chapter.query.filter_by(id=id).first()
 	project = Project.query.filter_by(id=chapter.project_id).first()
-	if current_user.is_anonymous:
+	'''if current_user.is_anonymous:
 		return redirect(url_for('index'))
 	if current_user.is_authenticated:
 		if current_user.is_superuser() == False:
 			if project.author is not current_user:
-				return redirect(url_for('index'))
+				return redirect(url_for('index'))'''
 	db.session.delete(chapter)
 	db.session.commit()
 	return redirect(url_for('project', id=project.id, title=project.title))
@@ -368,12 +368,12 @@ def delete_chapter(id):
 def add_to_library(id):
 	user = User.query.filter_by(username=current_user.username).first()
 	project = Project.query.filter_by(id=id).first()
-	if current_user.is_anonymous:
+	'''if current_user.is_anonymous:
 		return redirect(url_for('index'))
 	if current_user.is_authenticated:
 		if current_user.is_superuser() == False:
 			if project.author is not current_user:
-				return redirect(url_for('index'))
+				return redirect(url_for('index'))'''
 	user.books.append(project)
 	db.session.commit()
 	return redirect(url_for('project', id=project.id, title=project.title))
