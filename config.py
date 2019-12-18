a8cd4367b1652697e3fa33c3b448114cd3296ac0
lootfee <lutfiayven@gmail.com>
2019-12-19 01:56:17 +0400

    edit

--- config.py
+++ config.py
@@ -1,16 +1,19 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__) )
+load_dotenv()
 
 class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+	SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	POSTS_PER_PAGE = 20
 	UPLOADED_PHOTOS_DEST = os.getcwd() + '/app/static/uploads'
-	MAIL_SERVER = os.environ.get('MAIL_SERVER')
-	MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-	MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-	ADMINS = ['admin@writerrific.com']
+	MAIL_SERVER = os.getenv('MAIL_SERVER')
+	MAIL_PORT = int(os.getenv('MAIL_PORT') or 25)
+	MAIL_USE_TLS = os.getenv('MAIL_USE_TLS') is not None
+	MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+	MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+	ADMINS = ['support@writerrific.com']
 	
